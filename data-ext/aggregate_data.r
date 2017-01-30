2288fab4d8f8f6ba9963de1c5e3b3ccad143ce24
James Dunham <james@jdunham.io>
2017-01-30 11:14:35 -0500

    fix missing rows in aggregate_data

--- data-ext/aggregate_data.r
+++ data-ext/aggregate_data.r
@@ -1,22 +1,21 @@
 library(data.table)
-aggregates = foreign::read.dta('~/Dropbox (MIT)/Survey Database/DGIRT Data/GSS_long160412.dta')
+library(dplyr)
+path = "~/Dropbox (MIT)/Survey Database/DGIRT Data/GSS_long160412.dta"
+aggregates = foreign::read.dta(path)
 aggregates[sapply(aggregates, is.factor)] <- 
   lapply(aggregates[sapply(aggregates, is.factor)], as.character)
 setDT(aggregates)
-aggregates[, `:=`(state = STATEAB, STATEAB = NULL,
-                      n_grp = N, N = NULL,
-                      s_grp = S, S = NULL,
-                      female = gender, gender = NULL)]
+setnames(aggregates, c("STATEAB", "N", "S", "gender", "race"),
+         c("state", "n_grp", "s_grp", "female", "race3"))
 aggregates <- as.data.frame(aggregates)
 aggregates$year <- as.integer(aggregates$year)
 aggregates <- aggregates[aggregates$year %in% 2006:2008, ]
-aggregates$race <- replace(aggregates$race, aggregates$race == "1", "white")
-aggregates$race <- replace(aggregates$race, aggregates$race == "2", "black")
-aggregates$race <- replace(aggregates$race, aggregates$race == "3", "other")
+aggregates$race3 <- replace(aggregates$race3, aggregates$race3 == "1", "white")
+aggregates$race3 <- replace(aggregates$race3, aggregates$race3 == "2", "black")
+aggregates$race3 <- replace(aggregates$race3, aggregates$race3 == "3", "other")
 aggregates$female <- replace(aggregates$female, aggregates$female == "0", "male")
 aggregates$female <- replace(aggregates$female, aggregates$female == "1", "female")
-dupe_rows <- duplicated(aggregates[, c("race", "year", "item", "state")])
-aggregates <- aggregates[!dupe_rows, ]
-aggregates <- aggregates[, c("race", "female", "year", "item", "state", "n_grp", "s_grp")]
-colnames(aggregates, "race", "race3")
+aggregates <- aggregates %>%
+  group_by_("year", "state", "race3", "female", "item") %>%
+  summarize(n_grp = sum(n_grp), s_grp = sum(s_grp))
 devtools::use_data(aggregates, overwrite = TRUE)
